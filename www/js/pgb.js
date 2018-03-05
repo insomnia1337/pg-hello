46075aa742fba2a488c911e643055ccdb850d036
s-115-14 <s-115-14@gumiforest.ae.krakow.pl>
2018-03-05 17:40:40 +0100

    add movement

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -37,3 +37,16 @@ function checkConnection() {
     alert('Connection type: ' + states[networkState]);
 }
 
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+
